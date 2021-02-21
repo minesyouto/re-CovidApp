1964b0985796180f94954ce3f36db9783fd582ff
minesyouto <minesyouto@users.noreply.github.com>
2021-02-21 21:45:44 +0900

    Initial Commit

---
+++ CovidApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidApp
+//
+//  Created by みねた on 2021/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
