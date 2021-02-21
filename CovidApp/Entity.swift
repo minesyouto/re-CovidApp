105f936127a69c450d6c47fcc26eea292c6aec36
minesyouto <minesyouto@users.noreply.github.com>
2021-02-21 22:18:32 +0900

    2

---
+++ CovidApp/Entity.swift
@@ -0,0 +1,15 @@
+
+
+struct  CovidInfo: Codable {
+    
+    struct  Total: Codable {
+        var pcr: Int
+        var positive: Int
+        var hospitalize: Int
+        var severe: Int
+        var death: Int
+        var discharge: Int
+    }
+}
+    
+
